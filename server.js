61023e3ef597033b2309c91e8663e9569fc14e2b
ur-moms-a-fortnite-dab <62729296+ur-moms-a-fortnite-dab@users.noreply.github.com>
2020-05-30 16:41:50 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const express=require('express')
+const morgan=require('morgan')
+const morganSetting=process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+const POKEDEX = require('./pokedex.json')
+
+const app = express()
+
+app.use(morgan(morganSetting))
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = "e94930e6-9d36-11ea-bb37-0242ac130002";
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+  
+})
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.get('/types', function handleGetTypes(req, res) {
+  res.json(validTypes)
+})
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+
+  res.json(response)
+})
+app.use((error, req, res, next) =>{
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
